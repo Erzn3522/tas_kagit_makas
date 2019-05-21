1d728ef6d5f55aa7e60d7bc419cd8e7285c5a2e7
Abdullah Erzin <49682480+Erzn3522@users.noreply.github.com>
2019-05-21 20:03:15 +0300

    Add files via upload

---
+++ Thread_Deneme.py
@@ -0,0 +1,16 @@
+from time import sleep
+from threading import Thread
+
+def tekrarla(ne, bekleme):
+    while True:
+        print (ne)
+        sleep(bekleme)
+
+if __name__ == '__main__':
+    dum = Thread(target = tekrarla, args = ("dum",1))
+    tis = Thread(target = tekrarla, args = ("tis",0.5))
+    ah = Thread(target = tekrarla, args = ("ah",3))
+
+    dum.start()
+    tis.start()
+    ah.start()
